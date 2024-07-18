18220f519d788bbd2d745fae23af3f3bbf850ba0
illsio <smasho@web.de>
2024-07-18 13:17:51 +0200

    change for local test

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import "dotenv/config";
 import {connect} from "mongoose";
 import app from "./server.js";
+const port = 3000;
 
 // eslint-disable-next-line no-process-env
-connect(process.env.MONGODB_URI);
+connect("mongodb+srv://datanarrator:lZHqFZS8U99B9xVj@cluster0.xqy6gwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 // eslint-disable-next-line no-process-env
 console.log(process.env.MONGODB_URI);
-app.listen(80);
+console.log(port);
+app.listen(port);
