ad92683d690ea8934420ee43a9258d6344dbc2ab
illsio <smasho@web.de>
2024-07-19 10:12:16 +0200

    local changes for docker testing 4

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import "dotenv/config";
 import {connect} from "mongoose";
 import app from "./server.js";
-const port = 3000;
 
 // eslint-disable-next-line no-process-env
-connect("mongodb+srv://datanarrator:lZHqFZS8U99B9xVj@cluster0.xqy6gwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+connect(process.env.MONGODB_URI);
 // eslint-disable-next-line no-process-env
 console.log(process.env.MONGODB_URI);
-console.log(port);
-app.listen(port);
+app.listen(80);
