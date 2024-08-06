4b61cdd552d79130f2fae3c3fca165cb5b067ca3
illsio <smasho@web.de>
2024-08-06 11:30:11 +0200

    miniio testing IV

--- utils/s3Actions.js
+++ utils/s3Actions.js
@@ -13,8 +13,8 @@ export function getSignedUrl (key) {
     const s3ObjectUrl = parseUrl(`https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`),
         presigner = new S3RequestPresigner({
             credentials: {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+                accessKeyId: "ROOTNAME",
+                secretAccessKey: "CHANGEME123"
             },
             region: process.env.AWS_REGION,
             sha256: Hash.bind(null, "sha256")
